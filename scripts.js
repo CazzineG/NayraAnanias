8a936e9b26507d2bae2dbf60b66cc465912877f8
Gustavo Cazzine <cazzinengustavo@gmail.com>
2024-07-29 12:32:24 -0300

    att1.1 estrutura

--- scripts.js
+++ scripts.js
@@ -11,3 +11,19 @@ function showNextReview() {
 }
 
 setInterval(showNextReview, 3000);
+
+function downloadPDF(pdfPath, fileName) {
+    const link = document.createElement('a');
+    link.href = pdfPath;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function openWhatsApp() {
+    const phoneNumber = '5519999670165'; // número de telefone 
+    const message = encodeURIComponent('Olá Nayra, gostaria de agendar um horário.'); // mensagem inicial
+    const url = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(url, '_blank');
+}
